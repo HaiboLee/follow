da9993b9af2b708b675729e9b45a6c630c54b99e
lihai <lihaiboloveyou@live.com>
2016-11-30 08:16:57 +0800

    foot

--- asset/js/state/play.js
+++ asset/js/state/play.js
@@ -1,11 +1,13 @@
 var playState = function (game) {
     let map,layer1;
     let girl,cursors;
+    let xfoot = new Array();
+    let yfoot = new Array();
     this.create = function () {
         game.stage.backgroundColor = '#2d2d2d';
         map = game.add.tilemap();
-        map.addTilesetImage('m','gridtiles',32,32);
-        layer1 = map.create('level1',50,38,32,32);
+        map.addTilesetImage('m','gridtiles',5,5);
+        layer1 = map.create('level1',160,120,5,5);
         layer1.scrollFactorX = 0.5;
         layer1.scrollFactorY = 0.5;
         layer1.resizeWorld();
@@ -13,40 +15,73 @@ var playState = function (game) {
 
         girl  = game.add.sprite(320,320,'girl');
         girl.animations.add('up',[0,1,2,3]);
-        girl.animation.play('up',12,true);
-        //girl.anchor.setTo(0.5);
-        map.putTile(1,Math.floor(girl.x/32),Math.floor(girl.y/32),layer1);
+        girl.animations.add('down',[20,21,22,23]);
+        girl.animations.add('left',[28,29,30,31]);
+        girl.animations.add('right',[12,13,14,15]);
+        girl.anchor.setTo(0.5,1);
+        map.putTile(1,Math.floor(girl.x/5),Math.floor(girl.y/5),layer1);
         cursors = this.input.keyboard.createCursorKeys();
         game.input.onDown.add(function () {
             map.removeTile(0,0,layer1);
         });
 
+        setInterval(function () {
+            if (xfoot.length!=0){
+                let x = xfoot.pop();
+                let y = yfoot.pop();
+                map.removeTile(x,y,layer1);
+            }
+        },300);
+
+        let camera = game.camera;
+
+        camera.follow(girl);
 
     }
     this.update = function () {
         if (cursors.up.isDown) {
             girl.y--;
-            let hasTile = map.hasTile(Math.floor((girl.x+16)/32),Math.floor((girl.y+16)/32),layer1);
+            girl.animations.play('up',12);
+            let x = Math.floor((girl.x)/5);
+            let y = Math.floor((girl.y)/5);
+            let hasTile = map.hasTile(x,y,layer1);
             if (!hasTile){
-                createTile(1,Math.floor(girl.x/32),Math.floor(girl.y/32),layer1);
+                xfoot.unshift(x);
+                yfoot.unshift(y);
+                createTile(1,x,y,layer1);
             }
         }else if(cursors.down.isDown){
             girl.y++;
-            let  hasTile = map.hasTile(Math.floor((girl.x+16)/32),Math.floor((girl.y+16)/32),layer1);
+            girl.animations.play('down',12);
+            let x = Math.floor((girl.x)/5);
+            let y = Math.floor((girl.y)/5);
+            let hasTile = map.hasTile(x,y,layer1);
             if (!hasTile){
-                createTile(1,Math.floor(girl.x/32),Math.floor(girl.y/32),layer1);
+                xfoot.unshift(x);
+                yfoot.unshift(y);
+                createTile(1,x,y,layer1);
             }
         }else if(cursors.right.isDown){
             girl.x++;
-            let  hasTile = map.hasTile(Math.floor((girl.x+16)/32),Math.floor((girl.y+16)/32),layer1);
+            girl.animations.play('right',12);
+            let x = Math.floor((girl.x)/5);
+            let y = Math.floor((girl.y)/5);
+            let hasTile = map.hasTile(x,y,layer1);
             if (!hasTile){
-                createTile(1,Math.floor(girl.x/32),Math.floor(girl.y/32),layer1);
+                xfoot.unshift(x);
+                yfoot.unshift(y);
+                createTile(1,x,y,layer1);
             }
         }else if(cursors.left.isDown){
             girl.x--;
-            let  hasTile = map.hasTile(Math.floor((girl.x+16)/32),Math.floor((girl.y+16)/32),layer1);
+            girl.animations.play('left',12);
+            let x = Math.floor((girl.x)/5);
+            let y = Math.floor((girl.y)/5);
+            let hasTile = map.hasTile(x,y,layer1);
             if (!hasTile){
-                createTile(1,Math.floor(girl.x/32),Math.floor(girl.y/32),layer1);
+                xfoot.unshift(x);
+                yfoot.unshift(y);
+                createTile(1,x,y,layer1);
             }
         }
     }
